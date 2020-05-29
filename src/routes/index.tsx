998d0f938533e0840cbda83cd05c3f66875f6824
Vitor Frota <vitorfrota@live.com>
2020-05-29 12:11:14 -0400

    23 - Rotas autenticadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/Signup';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
