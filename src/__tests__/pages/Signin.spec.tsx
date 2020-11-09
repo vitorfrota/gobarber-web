4561571263453ea637ccda4fbc1f35e8c1543ab5
Vitor Frota <vitorfrota@live.com>
2020-11-09 11:13:13 -0400

    iniciando os testes

---
+++ src/__tests__/pages/Signin.spec.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import SignIn from '../../pages/Signin';
+
+const mockedHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: mockedHistoryPush,
+  }),
+  Link: ({ children }: { children: React.ReactNode }) => children,
+}));
+
+jest.mock('../../hooks/auth', () => ({
+  useAuth: () => ({
+    signIn: jest.fn(),
+  }),
+}));
+
+describe('SignIn Page', () => {
+  it('should be able to sign in', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledTimes(1);
+    });
+  });
+});
